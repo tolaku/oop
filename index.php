008d656a9b4c0e16efcea3ff6ce8345f9de64f62
Anatol <tola_k@mail.ru>
2018-02-05 20:08:32 +0300

    2

--- index.php
+++ index.php
@@ -1,17 +1,55 @@
 <?php 
-include 'config.php';
-include 'classes/Page.php';
-include 'classes/Database.php';
+	// все страницы сайта
+	class Page {
+		public $title;
+		public $content;
+		public $footer;
 
-$page = new Page();
-if(isset($_GET['id'])){
-	// выводим полностью информации о статье
-}
-else{
-	// обращаемся к объекту Page в файле Page.php для вывода всех статей
-	$text = $page->get_all();
-	// выводим массив в html код, вызывая нашу функцию get_body(), укажем массив и второй параметр шаблон для вывода
-	$page->get_body($text, 'main');
-}
+		public function __construct($t, $c, $f){
+			$this->title = $t;
+			$this->content = $c;
+			$this->footer = $f;
+		}
 
- ?>
+		// выводит на экран строку
+		public function render_body(){
+			$str = '<h1>'.$this->title.'</h1>';
+			$str .= '<p>'.$this->content.'</p>';
+			$str .= '<p style="font-size:8px;">'.$this->footer.'</p>';
+
+			return $str;
+		}
+
+	}
+
+	class Index extends Page {
+		public $slide;
+		public $news;
+
+		public function __construct($t, $c, $f, $s, $n){
+			parent::__construct($t, $c, $f);
+			$this->slide = $s;
+			$this->news = $n;
+		}
+
+		public function render_body(){
+			$str = parent::render_body();
+			$str.= "<p>".$this->slide."</p>";
+			$str.= "<p>".$this->slide."</p>";
+
+			return $str;
+		}
+	}
+
+	// объект класса Page
+	$page = new Page('Page', 'hello i am page', 'footer');
+	echo $page->render_body();
+
+	//  объект нашего класса Index
+	$index = new Index('INDEX', 'HELLO i am index', 'footer', 'slide_show', 'news');
+
+	echo $index->render_body();
+
+	//echo $index->render_body();
+?>
+
