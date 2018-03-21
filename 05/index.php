f3614937efb21178cf9a060d5664d49969520b50
Anatol <tola_k@mail.ru>
2018-03-21 11:12:00 +0300

    5 lesson

---
+++ 05/index.php
@@ -0,0 +1,24 @@
+<?php
+
+class User {
+//    public $name;
+//    public $password;
+//    public $email;
+//    public $city;
+    private static $name;
+    
+    public static function setName($name1){
+        self::$name = $name1;
+    }
+    
+    public static function getName(){
+        return self::$name;
+    }
+
+}
+
+User::setName('Анатолий');
+echo User::getName();
+
+
+
