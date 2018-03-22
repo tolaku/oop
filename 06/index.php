051ac1b5a0f0fb00c9cb43118b58ff9e103eddda
Anatol <tola_k@mail.ru>
2018-03-22 09:13:35 +0300

    6 lesson

---
+++ 06/index.php
@@ -0,0 +1,62 @@
+<?php
+
+class User {
+    public $name;
+    public $password;
+    public $email;
+    public $city;
+    
+    function __construct($name, $password, $email, $city) {
+        $this->name = $name;
+        $this->password = $password;
+        $this->email = $email;
+        $this->city = $city;
+    }
+    
+    function getInfo(){
+        $information = "{$this->name}"
+        . "{$this->password}"
+        . "{$this->email}"
+        . "{$this->city}";
+        return $information;
+    }
+}
+
+$user1 = new User("Анатолий ", "654654 ", "@mail.ru ", "Пинск");
+//echo $user1->getInfo();
+
+Class Moderator extends User{
+    protected $info;
+    public $right;
+    
+    function __construct($name, $password, $email, $city, $info, $right) {
+        parent::__construct($name, $password, $email, $city);
+        $this->info = $info;
+        $this->right = $right;
+    }
+    
+    function getInfo() {
+        $information = parent::getInfo();
+        $information .= "{$this->info}" . "{$this->right}";
+        
+        return $information;
+    }
+}
+
+$moderator = new Moderator(" Андрей ", "111111 ", "tut@mail.ru ", "Стытычево ", "Модератор ", "Тру");
+echo $moderator->getInfo();
+
+//class Test {
+//    protected $info;
+//}
+//
+//class Test2 extends Test{
+//    public function test(){
+//        $this->info = "info";
+//        echo $this->info;
+//    }
+//}
+//
+//$test2 = new Test2();
+//$test2->info = "test2";
+//$test2->test();
